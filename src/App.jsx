cfa3587d635ff6e5a4cd3a4bb2e27c4bf2f6d426
GIrigoin <guilleirigoin@hotmail.com>
2024-07-10 17:54:09 -0300

    Dia tres: amigandose con MUI, renderizado de transcripcion y navegacion del audio a traves de la transcripcion

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+// import { palette } from "@mui/system";
+import {
+  blueGrey,
+  purple,
+  grey,
+  yellow,
+  lightBlue,
+} from "@mui/material/colors";
 // import ReactAudioPlayer from "react-audio-player";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
@@ -24,72 +33,77 @@ function App() {
       const { data } = await axios(
         "https://res.cloudinary.com/dyt4p8agq/raw/upload/v1720479186/xol1azvhus4oznorce7l.json"
       );
-      console.log(data);
       setTranscription(data);
     };
 
     getTranscription();
   }, []);
 
-  const handleButtonClick = async () => {
-    // const player = new Audio(
-    //   "https://res.cloudinary.com/dyt4p8agq/video/upload/v1720479201/epn2vs0g0wxnowelcjef.wav"
-    // );
-    // await player.play();
-    myRef.current.audio.current.currentTime = 30;
+  const mapTranscription = () => {
+    return transcription.map((element, index) => (
+      <Paper
+        elevation={2}
+        sx={{
+          my: 1,
+          p: 1,
+          maxWidth: "60%",
+          bgcolor: element.role === "agent" ? lightBlue[100] : yellow[100],
+          alignSelf: element.role === "agent" ? "flex-start" : "flex-end",
+        }}
+        onClick={() => handleButtonClick(index)}
+      >
+        <Typography variant="body2" color={grey[900]}>
+          {element.content}
+        </Typography>
+      </Paper>
+    ));
+  };
+  const handleButtonClick = async (index) => {
+    myRef.current.audio.current.currentTime = transcription[index].start;
     myRef.current.audio.current.play();
   };
 
   return (
     <>
       <Container maxWidth="md">
-        <Typography variant="h2" color="initial">
-          Transcription Viewer V1
-        </Typography>
+        <Box bgcolor={purple[200]} padding={3} borderRadius={4}>
+          <Typography
+            variant="h3"
+            color={blueGrey[900]}
+            fontWeight={"bold"}
+            my={4}
+          >
+            Transcription Viewer V1
+          </Typography>
 
-        {/* {myRef?.current?.audio && (
-          <AudioVisualizer
-            // ref={visualizerRef}
-            blob={new Blob(myRef.current.audio.current.audioTracks)}
-            width={500}
-            height={75}
-            barWidth={1}
-            gap={0}
-            barColor={"#f76565"}
-          />
-        )} */}
-        {/* <div
-          style={{
-            width: "100%",
-            maxWidth: 600,
-            height: "200px",
-            margin: "0 auto",
-          }}
-        > */}
-        <Box height={200} maxHeight={480} bgcolor={"secondary.light"}>
-          {myRef?.current?.audio && (
-            <AudioVisual audio={myRef?.current?.audio} />
-          )}
+          <Paper elevation={4} sx={{ my: 2, borderRadius: 3 }}>
+            <Box height={200} bgcolor={blueGrey[800]}>
+              {myRef?.current?.audio && (
+                <AudioVisual audio={myRef?.current?.audio} />
+              )}
+            </Box>
+            <AudioPlayer
+              src="https://res.cloudinary.com/dyt4p8agq/video/upload/v1720479201/epn2vs0g0wxnowelcjef.wav"
+              showSkipControls
+              showJumpControls={false}
+              ref={myRef}
+              crossOrigin="*"
+            />
+          </Paper>
+
+          <Box my={4}>
+            <Paper
+              elevation={4}
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              {mapTranscription()}
+            </Paper>
+          </Box>
         </Box>
-        {/* </div> */}
-
-        <AudioPlayer
-          src="https://res.cloudinary.com/dyt4p8agq/video/upload/v1720479201/epn2vs0g0wxnowelcjef.wav"
-          showSkipControls
-          showJumpControls={false}
-          ref={myRef}
-          crossOrigin="*"
-        />
-        {/* <ReactAudioPlayer
-          src="https://res.cloudinary.com/dyt4p8agq/video/upload/v1720479201/epn2vs0g0wxnowelcjef.wav"
-          controls
-          ref={(element) => {
-            this.rap = element;
-          }}
-        ></ReactAudioPlayer> */}
-        <Button variant="contained" onClick={handleButtonClick}>
-          Sonidete
-        </Button>
       </Container>
     </>
   );
